2074fced8288517f4525e5fb4214068f76915afa
Mamun <simuljsr7@gmail.com>
2020-05-03 23:39:26 -0400

    created homepage

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 
-
+import HomePage from './homepage.component'
 
 import './App.css';
 
 function App() {
   return (
 
-    <div className="App">
-     
+    <div >
+     <HomePage/>
  
     </div>
   );
